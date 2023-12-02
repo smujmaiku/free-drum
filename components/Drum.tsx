b5f9a5fd4c3328d18d3d84ba331def9cb2b77419
Michael Szmadzinski <smujmaiku@gmail.com>
2023-12-02 12:45:14 -0500

    cleaner starts

--- components/Drum.tsx
+++ components/Drum.tsx
@@ -1,19 +1,42 @@
 import { useEffect, useRef } from "react";
 
+const GAMEPAD_TIMER = 10;
+
 export interface DrumPropsI {
   name?: string;
   button: number;
   src: string;
   type?: string;
+  start?: number;
+  end?: number;
 }
 
 export function Drum(props: DrumPropsI) {
-  const { button, src, type } = props;
+  const { button, src, type, start = 0, end = Infinity } = props;
   const ref = useRef<HTMLAudioElement>(null!);
 
   useEffect(() => {
-    let active = false;
+    const el = ref.current;
 
+    let endTimer: NodeJS.Timeout | undefined;
+    const handlePlay = () => {
+      clearTimeout(endTimer);
+      el.currentTime = start;
+
+      const endTime = Math.floor((end - start) * 1000);
+      if (endTime <= 0) {
+        el.pause();
+        return;
+      } else if (endTime === Infinity) {
+        return;
+      }
+
+      endTimer = setTimeout(() => {
+        el.pause();
+      }, endTime);
+    }
+
+    let active = false;
     const upkeep = () => {
       const gamepads = navigator.getGamepads();
       for (const gamepad of Object.values(gamepads)) {
@@ -21,22 +44,23 @@ export function Drum(props: DrumPropsI) {
         if (!id || !buttons) return;
 
         const { value } = gamepad?.buttons[button] || {};
-        const el = ref.current;
 
         if (value && !active && el) {
-          el.currentTime = 0;
+          el.pause();
           el.play();
         }
         active = Boolean(value);
       }
     }
 
-    const timer = setInterval(upkeep, 10);
+    el.addEventListener('play', handlePlay)
+    const timer = setInterval(upkeep, GAMEPAD_TIMER);
 
     return () => {
+      el.removeEventListener('play', handlePlay)
       clearInterval(timer)
     }
-  }, [button]);
+  }, [button, start, end]);
 
   return (
     <audio ref={ref}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,35 +3,41 @@ import { Drum } from '@/components/Drum'
 export default function Home() {
 	return (
 		<main>
+			Free Drum
 			<Drum
 				name="tom"
 				button={0}
 				src="https://freewavesamples.com/files/Floor-Tom-1.wav"
 				type="audio/wav"
+				start={0.1}
 			/>
 			<Drum
 				name="snare"
 				button={1}
 				src="https://freewavesamples.com/files/Ensoniq-ESQ-1-Snare.wav"
 				type="audio/wav"
+				start={0.1}
 			/>
 			<Drum
 				name="ride"
 				button={2}
 				src="https://freewavesamples.com/files/Ensoniq-SQ-1-Ride-Cymbal.wav"
 				type="audio/wav"
+				start={0.1}
 			/>
 			<Drum
 				name="crash"
 				button={3}
 				src="https://freewavesamples.com/files/Crash-Cymbal-1.wav"
 				type="audio/wav"
+				start={0.1}
 			/>
 			<Drum
 				name="kick"
 				button={4}
 				src="https://freewavesamples.com/files/Bass-Drum-3.wav"
 				type="audio/wav"
+				start={0.1}
 			/>
 		</main>
 	)
